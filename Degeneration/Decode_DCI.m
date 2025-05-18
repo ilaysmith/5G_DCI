68c7c5c0ae4e0516190ae4306414d0b5859ccc5a
Ilya <129608057+ilaysmith@users.noreply.github.com>
2025-05-18 21:47:43 +0300

    Add files via upload

---
+++ Degeneration/Decode_DCI.m
@@ -0,0 +1,19 @@
+% function Decode_DCI for dci sequence 
+function get_DM = Decode_DCI(received_codeword, crc_type)
+arguments
+    received_codeword
+    crc_type
+end
+
+% CRC   use crc_type = 'crc24c', bitstream = received_codeword . rateRecovery - взято
+% у Dimach24
+received_codeword = rateRecovery(received_codeword);
+
+% Channel decoding. polarDecoding - взято у Dimach24
+received_codeword = polarDecoding(received_codeword); 
+
+% verifeParyity - взято у Dimach24
+get_DM = verifyParity(received_codeword, crc_type);
+
+%get_DM = received_codeword;
+end
