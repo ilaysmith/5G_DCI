c57ae422d321561927ff15e7b47180906b9450d8
borodina <yasenskaya1925@gmail.com>
2025-05-26 15:55:11 +0300

    Add files via upload

--- Generation/Encode_DCI.m
+++ Generation/Encode_DCI.m
@@ -13,21 +13,31 @@ arguments
 
 end
 
-
-% CRC   use crc_type = 'crc24c', bitstream = dci . attachParityBits - взято
-% у Валентина
+% crc attachment
+% CRC   use crc_type = 'crc24c', bitstream = dci 
 codeword = attachParityBits(dci,crc_type);
 
-% Channel coding. polarCoding - взято у Валентина. Добавил динамическую
-% длину и исправил цикл с формированием G_N
+% crc attachment test
+%bitscrcPad = nrCRCEncode([ones(24,1,class(dci.'));dci.'],'24C');  % First prepend 1s
+bitscrcPad = nrCRCEncode(dci.','24C');  %matlab function
+%cVec = bitscrcPad(25:end,1); % Then, after calculating the CRC, remove the 1s
+isequal(codeword, bitscrcPad.');
+%isequal(codeword, cVec.') ;
+
+% Channel coding
 codeword = polarCoding(codeword); 
 
-%codewordnew = nrPolarEncode(codeword,length(codeword)); 
-%isequal(codeword,codewordnew)
+% Channel coding test
+encOut = nrPolarEncode(bitscrcPad, 864); %matlab function
+isequal(codeword, encOut.');
 
-% Rate matching - взято у Валентина
+% Rate matching 
 codeword = rateMatching(codeword);
 
+% Rate matching test
+K = length(bitscrcPad);
+dciCW = nrRateMatchPolar(encOut,K,864); %matlab function
+isequal(codeword, dciCW.');
 
 end
 
