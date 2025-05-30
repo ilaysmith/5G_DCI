bc96bd48237528306e1b4bab5c4a1c83c6b35d6a
borodina <yasenskaya1925@gmail.com>
2025-05-30 21:48:17 +0300

    Update Encode_DCI.m

--- Generation/Encode_DCI.m
+++ Generation/Encode_DCI.m
@@ -17,27 +17,11 @@ end
 % CRC   use crc_type = 'crc24c', bitstream = dci 
 codeword = attachParityBits(dci,crc_type);
 
-% crc attachment test
-%bitscrcPad = nrCRCEncode([ones(24,1,class(dci.'));dci.'],'24C');  % First prepend 1s
-bitscrcPad = nrCRCEncode(dci.','24C');  %matlab function
-%cVec = bitscrcPad(25:end,1); % Then, after calculating the CRC, remove the 1s
-isequal(codeword, bitscrcPad.');
-%isequal(codeword, cVec.') ;
-
 % Channel coding
 codeword = polarCoding(codeword); 
 
-% Channel coding test
-encOut = nrPolarEncode(bitscrcPad, 864); %matlab function
-isequal(codeword, encOut.');
-
 % Rate matching 
 codeword = rateMatching(codeword);
-
-% Rate matching test
-K = length(bitscrcPad);
-dciCW = nrRateMatchPolar(encOut,K,864); %matlab function
-isequal(codeword, dciCW.');
 
 end
 
