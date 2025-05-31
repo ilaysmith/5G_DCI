a10c9951af9b0d7fee6741db40eafc634a6c71b7
Ilya <129608057+ilaysmith@users.noreply.github.com>
2025-06-01 00:12:46 +0300

    Add files via upload

--- Degeneration/Decode_DCI.m
+++ Degeneration/Decode_DCI.m
@@ -7,13 +7,13 @@ end
 
 % CRC   use crc_type = 'crc24c', bitstream = received_codeword . rateRecovery - взято
 % у Dimach24
-received_codeword = rateRecovery_2(received_codeword); % корректно. получаем тот же результат
+received_codeword = rateRecovery(received_codeword); 
 
 % Channel decoding. polarDecoding - взято у Dimach24
-received_codeword = polarDecoding_2(received_codeword); % не корректно. результат нули
+received_codeword = polarDecoding_2(received_codeword); 
 
 % verifeParity - взято у Dimach24
-get_DM = verifyParity_2(received_codeword, crc_type);
+get_DM = verifyParity(received_codeword, crc_type);
 
 %get_DM = received_codeword;
 end
