0245c1555132967b9aaff1d69df83a79bbd35123
borodina-E <yasenskaya1925@gmail.com>
2025-06-01 20:56:19 +0300

    Revert "output corrected recovery"
    This reverts commit 66cd2edf9c69f707cd40f391ed3dab68bd0c8141.

--- Generation/Encode_DCI.m
+++ Generation/Encode_DCI.m
@@ -18,7 +18,7 @@ end
 codeword = attachParityBits(dci,crc_type);
 
 % Channel coding
-codeword = polarCoding_2(codeword); 
+codeword = polarCoding(codeword); 
 
 % Rate matching 
 codeword = rateMatching(codeword);
