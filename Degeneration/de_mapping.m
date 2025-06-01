102b17417d367508502af3323dcc625b9dbb3fd4
Ilya <129608057+ilaysmith@users.noreply.github.com>
2025-06-01 13:36:16 +0300

    Add files via upload

--- Degeneration/de_mapping.m
+++ Degeneration/de_mapping.m
@@ -1,11 +1,10 @@
-function [pdcch_symbols] = de_mapping(resource_grid, nID,coreset_config)
+function [pdcch_symbols,dmrs_symbols] = de_mapping(resource_grid,coreset_config)
     % 1. Извлечение RE PDCCH из CORESET
-
-    pdcch_symbols = extract_pdcch_from_coreset(resource_grid, coreset_config);
+    [pdcch_symbols,dmrs_symbols] = extract_pdcch_from_coreset(resource_grid, coreset_config);
   
 end
 
-function pdcch_symbols = extract_pdcch_from_coreset(resource_grid, coreset_config)
+function [pdcch_symbols,dmrs_symbols] = extract_pdcch_from_coreset(resource_grid, coreset_config)
 
     pdcch_symbols = []; 
     reg_count = 0;
@@ -13,8 +12,29 @@ function pdcch_symbols = extract_pdcch_from_coreset(resource_grid, coreset_confi
    % coreset_config.freq_range(2) = 63;
     %coreset_config.start_symbol = 7;
    % coreset_config.duration = 2;
+
+
+    % 1. Get DM-RS
+        dmrs_symbols = [];
+    for rb = coreset_config.freq_range(1) : coreset_config.freq_range(2)
+         for sym = coreset_config.start_symbol -1 : coreset_config.start_symbol + coreset_config.duration - 2
+           
+            % Извлечение 9 полезных RE из REG (исключая не DM-RS)
+            re_in_reg = [];
+            for sc = 1:12
+                if mod(sc-1, 4) == 0 % DM-RS
+                    re_pos = rb * 12 + sc;
+                    re_in_reg = [re_in_reg; resource_grid(re_pos, sym + 1)];
+                end
+            end
+            dmrs_symbols = [dmrs_symbols; re_in_reg];
+            reg_count = reg_count + 1;
+        end
+    end
+
     
-    % Non-interleaved маппинг (для CORESET0)
+    
+    % 2. Non-interleaved маппинг (для CORESET0) get pdcch 
     for rb = coreset_config.freq_range(1) : coreset_config.freq_range(2)
         for sym = coreset_config.start_symbol -1 : coreset_config.start_symbol + coreset_config.duration - 2
            
